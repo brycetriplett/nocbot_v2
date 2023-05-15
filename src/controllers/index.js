25615ea67f94abf13e87e672233bb27f3ad62873
Bryce Triplett <brycetriplett@gmail.com>
2023-05-15 12:38:36 -0700

    .

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,4 +1,5 @@
 module.exports = {
   errorController: require("./errorController"),
   telradController: require("./telradController"),
+  pushoverController: require("./pushoverController"),
 };
--- src/controllers/pushoverController.js
+++ src/controllers/pushoverController.js
@@ -4,8 +4,6 @@ const pushoverBlocks = require("@blocks/pushoverBlocks.js");
 const pushoverController = async ({ command, say, respond }) => {
   let [method, option] = command.text.split(" ");
 
-  console.log(`${method} ${option}`);
-
   if (method === "pause" && option && !/^\d+$/.test(option)) {
     respond("Invalid time value for notification pause length");
     return;
@@ -36,8 +34,7 @@ const pushoverController = async ({ command, say, respond }) => {
       respond("incorrect command");
       return;
   }
-  say("we got here");
-  // say(pushoverBlocks.defaultBlocks({ command, result, header }));
+  say(pushoverBlocks.defaultBlocks({ command, result, header }));
 };
 
 module.exports = pushoverController;
