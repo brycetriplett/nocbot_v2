f2a80edfa6a9f9507783f7d2e90c71fa5d326d3f
Bryce Triplett <brycetriplett@gmail.com>
2023-05-15 12:35:10 -0700

    .

--- src/controllers/pushoverController.js
+++ src/controllers/pushoverController.js
@@ -4,6 +4,8 @@ const pushoverBlocks = require("@blocks/pushoverBlocks.js");
 const pushoverController = async ({ command, say, respond }) => {
   let [method, option] = command.text.split(" ");
 
+  console.log(`${method} ${option}`);
+
   if (method === "pause" && option && !/^\d+$/.test(option)) {
     respond("Invalid time value for notification pause length");
     return;
