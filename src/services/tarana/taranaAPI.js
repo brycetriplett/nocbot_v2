ad36d09e0392da7e6f08f3fb7f11a50b76a76e19
Bryce Triplett <brycetriplett@gmail.com>
2024-01-22 11:24:16 -0800

    troubleshooting tarana commands

--- src/services/tarana/taranaAPI.js
+++ src/services/tarana/taranaAPI.js
@@ -3,10 +3,15 @@ const axios = require("axios");
 const hosturl = process.env.TARANA_API_URL;
 const api_key = process.env.TARANA_API_KEY;
 
+const headers = {
+  accept: "application/json",
+  "x-api-key": api_key,
+};
+
 const startSpeedTest = (serial) =>
   axios({
     method: "POST",
-    headers: { "x-api-key": api_key },
+    headers: headers,
     url: `${hosturl}/v1/network/radios/${serial}/speed-test`,
   }).then((response) => {
     return response.data.message;
