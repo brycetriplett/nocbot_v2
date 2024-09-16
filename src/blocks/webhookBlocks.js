134386dc70bd2c9a23eac3d4cf5a8e8f37b1f4df
Bryce Triplett <brycetriplett@gmail.com>
2024-09-16 10:04:51 -0700

    editing blocks for webhook

--- src/blocks/webhookBlocks.js
+++ src/blocks/webhookBlocks.js
@@ -22,15 +22,7 @@ const webhookBlocks = ({
       fields: [
         {
           type: "mrkdwn",
-          text: `*Operator*: ${significantData.Operator}`,
-        },
-        {
-          type: "mrkdwn",
           text: `*Site*: ${significantData.Site}`,
-        },
-        {
-          type: "mrkdwn",
-          text: `*Hostname*: ${significantData["Device Hostname"]}`,
         },
         {
           type: "mrkdwn",
@@ -39,25 +31,6 @@ const webhookBlocks = ({
         {
           type: "mrkdwn",
           text: `*Sector*: ${significantData.Sector}`,
-        },
-        {
-          type: "mrkdwn",
-          text: `*Device Reported Reason*: ${significantData["Device Reported Reason"]}`,
-        },
-        {
-          type: "mrkdwn",
-          text: `*Disconnect Reason*: ${significantData["Disconnect Reason"]}`,
-        },
-      ],
-    },
-    {
-      type: "context",
-      elements: [
-        {
-          type: "mrkdwn",
-          text: `*Alert Status:* ${alertStatus}\n*Alert Description:* ${alertDescription}\n*Created At:* ${new Date(
-            alertCreatedAt
-          ).toLocaleString()}`,
         },
       ],
     },
