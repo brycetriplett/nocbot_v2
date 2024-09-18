d1f1f568538ac7dca807c02cd7d420d334cc62f2
Bryce Triplett <brycetriplett@gmail.com>
2024-09-18 15:04:15 -0700

    .

--- src/controllers/webhookController.js
+++ src/controllers/webhookController.js
@@ -9,12 +9,11 @@ const webhookController = async (req, res, postToSlack) => {
   // check if the serial value is actually a serial or test data
   const serial = req.body.significantData["Device Serial Number"];
 
-  if (/^[A-Z0-9]{8,12}$/.test(serial)) {
+  if (/^[A-Z0-9]{8,16}$/.test(serial)) {
     // if serial, grab config
     const result = await taranaAPI.getDeviceConfig(serial);
 
     // turn all white space in the notes into spaces
-    // add to request body
     req.body.notes = result.data.notes.replace(/\s+/g, " ").trim();
   } else {
     // add dummy data for api testing
